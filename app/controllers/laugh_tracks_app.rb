feff760c5d6fa569fe76d5b1cf82e8b9b296c59f
abenetka <ali.benetka@gmail.com>
2018-12-01 13:54:49 -0700

    Changes params conditional to load statistics by age

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -5,7 +5,7 @@ set :root, File.expand_path("..", __dir__)
   get '/comedians' do
     if params[:age]
       @comedians = Comedian.where(age: params[:age])
-      @specials = Special.where(comedian_id: params[:id])
+      @specials = Special.where(comedian_id: @comedians.ids)
     else
       @comedians = Comedian.all
       @specials = Special.all
